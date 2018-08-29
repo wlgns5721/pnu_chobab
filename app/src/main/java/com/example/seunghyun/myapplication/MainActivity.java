775cfd296a189d28e381105211e8393d1b2c141f
panna4590 <suzume213@gmail.com>
2018-08-29 17:59:51 +0900

    first commit

---
+++ app/src/main/java/com/example/seunghyun/myapplication/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.seunghyun.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class MainActivity extends Activity
+        implements SurfaceHolder.Callback {
+    @SuppressWarnings("deprecation")
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    Button button;
+    String str;
+
+    @SuppressWarnings("deprecation")
+    Camera.PictureCallback jpegCallback;
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture(null, null, jpegCallback);
+            }
+        });
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+
+        jpegCallback = new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                FileOutputStream outStream = null;
+                try {
+                    str = String.format( "/sdcard/%d.jpg", System.currentTimeMillis() );
+                    outStream = new FileOutputStream(str);
+
+                    outStream.write(data);
+                    outStream.close();
+                }
+
+                catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+                }
+
+                Toast.makeText(getApplicationContext(),
+                        "Picture Saved", Toast.LENGTH_LONG).show();
+                refreshCamera();
+
+                Intent intent = new Intent(MainActivity.this,
+                        ResultActivity.class);
+                intent.putExtra("strParamName", str);
+                startActivity(intent);
+            }
+        };
+    }
+
+
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
+
+        try {
+            camera.stopPreview();
+        }
+
+        catch (Exception e) {
+        }
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }
+        catch (Exception e) {
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        camera = Camera.open();
+        camera.stopPreview();
+        Camera.Parameters param = camera.getParameters();
+        param.setRotation(90);
+        camera.setParameters(param);
+
+        try {
+
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }
+
+        catch (Exception e) {
+            System.err.println(e);
+            return;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder,
+                               int format, int width, int height) {
+        refreshCamera();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+}
