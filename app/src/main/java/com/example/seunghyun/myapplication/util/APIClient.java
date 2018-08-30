cb7d711c6454542034fc3bd7fe84a6c506be1ff4
wlgns5721 <wlgns5721@gmail.com>
2018-08-30 15:18:55 +0900

    camera modify

---
+++ app/src/main/java/com/example/seunghyun/myapplication/util/APIClient.java
@@ -0,0 +1,38 @@
+package com.example.seunghyun.myapplication.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Ji Hoon on 2018-08-30.
+ */
+
+public class APIClient {
+    private static Retrofit retrofit = null;
+    private static String token = null;
+    public static Retrofit getInstance() {
+        if(retrofit==null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://172.20.10.5:5000")
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static void setToken(String _token) {
+        token = _token;
+    }
+
+    public static String getToken() {
+        return token;
+    }
+
+
+}
